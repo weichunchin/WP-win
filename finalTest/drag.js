6fb507ed7811456dc236025e335fc3304dc4a4bc
weichunchin <bill940813@gmail.com>
2015-06-20 18:16:56 +0800

    uplaod learn

---
+++ finalTest/drag.js
@@ -0,0 +1,47 @@
+function AllowDrop(event){
+    event.preventDefault();
+}
+function Drag(event){
+    event.dataTransfer.setData("text",event.currentTarget.id);
+
+}
+function Drop(event){
+    event.preventDefault();
+    var data=event.dataTransfer.getData("text");
+
+    event.currentTarget.appendChild(document.getElementById(data));
+
+    if(event.currentTarget.id==="Box1"){
+    $("#moonResult").attr("src","image/moon6.png");
+    $("#mResult").text("下弦月");
+    }
+    if(event.currentTarget.id==="Box2"){
+    $("#moonResult").attr("src","image/moon5.png");
+    $("#mResult").text("虧凸月");
+    }
+    if(event.currentTarget.id==="Box3"){
+    $("#moonResult").attr("src","image/moon4.png");
+    $("#mResult").text("滿月");
+    }
+    if(event.currentTarget.id==="Box4"){
+    $("#moonResult").attr("src","image/moon3.png");
+    $("#mResult").text("盈凸月");
+    }
+    if(event.currentTarget.id==="Box5"){
+    $("#moonResult").attr("src","image/moon2.png");
+    $("#mResult").text("上弦月");
+    }
+    if(event.currentTarget.id==="Box6"){
+    $("#moonResult").attr("src","image/moon1.png");
+    $("#mResult").text("新月");
+    }
+    if(event.currentTarget.id==="Box7"){
+    $("#moonResult").attr("src","image/moon8.png");
+    $("#mResult").text("朔月");
+    } 
+    if(event.currentTarget.id==="Box8"){
+    $("#moonResult").attr("src","image/moon7.png");
+    $("#mResult").text("殘月");
+    }
+}
+
